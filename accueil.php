510818095591f4d534333e94a478a6fee64d63fa
Lalilaaa <128609020+Lalilaaa@users.noreply.github.com>
2023-03-22 15:01:15 +0100

    Create accueil.php

---
+++ accueil.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+  <head>
+    <meta charset="utf-8">
+    <title>JDR</title>
+    <link rel = "stylesheet" href = "style.css">
+  </head>
+
+  <head>
+	<ul class="menu">
+   	  <li>
+            <a href="http://pams.script.univ-paris-diderot.fr/~bousseaue/Projet/accueil.php"> <img src="https://club-jdr.fr/wp-content/uploads/2021/12/jdr.png" width=50 height=50 > </a>
+   	 </li>
+    	 <li>
+            <a href="http://pams.script.univ-paris-diderot.fr/~bousseaue/Projet/jouer.php">Jouer</a>
+   	 </li>
+   	 <li>
+            <a href="http://pams.script.univ-paris-diderot.fr/~bousseaue/Projet/monPerso.php">monPerso</a>
+         </li>
+         <li>
+            <a href="http://pams.script.univ-paris-diderot.fr/~bousseaue/Projet/sInscrire.php">connection/inscription</a>
+         </li>
+        </ul>
+  </head>
+
+  <body>
+    <h1> Accueil </h1>
+  </body>
+
+</html>
